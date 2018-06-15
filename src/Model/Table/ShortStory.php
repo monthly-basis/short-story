f9351a6909c4302fea87027d642b695a535b54fc
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-06-15 03:39:42 +0000

    Write insert method

--- src/Model/Table/ShortStory.php
+++ src/Model/Table/ShortStory.php
@@ -16,4 +16,26 @@ class ShortStory
     {
         $this->adapter = $adapter;
     }
+
+    public function insert(
+        string $title,
+        string $body
+    ) {
+        $sql = '
+            INSERT
+              INTO `short_story` (
+                   `title`, `body`
+                   )
+            VALUES (?, ?)
+                 ;
+        ';
+        $parameters = [
+            $title,
+            $body,
+        ];
+        return $this->adapter
+                    ->query($sql)
+                    ->execute($parameters)
+                    ->getGeneratedValue();
+    }
 }
