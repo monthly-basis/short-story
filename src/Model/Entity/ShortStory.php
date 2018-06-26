c3e9eb53bf3620b216ad88640809e255d541da0a
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-06-26 07:39:48 +0000

    Add getters and setters

--- src/Model/Entity/ShortStory.php
+++ src/Model/Entity/ShortStory.php
@@ -3,7 +3,40 @@ namespace LeoGalleguillos\ShortStory\Model\Entity;
 
 class ShortStory
 {
+    protected $body;
     protected $shortStoryId;
     protected $title;
-    protected $body;
+
+    public function getBody() : string
+    {
+        return $this->body;
+    }
+
+    public function getShortStoryId() : int
+    {
+        return $this->shortStoryId;
+    }
+
+    public function getTitle() : title
+    {
+        return $this->title;
+    }
+
+    public function setBody(string $body)
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    public function setShortStoryId(int $shortStoryId)
+    {
+        $this->shortStoryId = $shortStoryId;
+        return $this;
+    }
+
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+        return $this;
+    }
 }
