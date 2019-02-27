f4601a9b1e5996f83331372926ffe0cefe20ce05
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-02-27 14:23:47 +0000

    Set user id when building short story

--- src/Model/Factory/ShortStory.php
+++ src/Model/Factory/ShortStory.php
@@ -12,18 +12,13 @@ class ShortStory
         $this->shortStoryTable = $shortStoryTable;
     }
 
-    /**
-     * Build from array.
-     *
-     * @param array $array
-     * @return ShortStoryEntity\ShortStory
-     */
     public function buildFromArray(array $array): ShortStoryEntity\ShortStory
     {
         $shortStoryEntity = new ShortStoryEntity\ShortStory();
         $shortStoryEntity->setBody($array['body'])
                          ->setShortStoryId($array['short_story_id'])
-                         ->setTitle($array['title']);
+                         ->setTitle($array['title'])
+                         ->setUserId((int) $array['user_id']);
 
         return $shortStoryEntity;
     }
