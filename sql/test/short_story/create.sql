cc750940518c0bb02e37c114071139ec31dd6061
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-07-09 08:03:49 +0000

    Add created and deleted columns to `short_story` table

--- sql/test/short_story/create.sql
+++ sql/test/short_story/create.sql
@@ -3,5 +3,9 @@ CREATE TABLE `short_story` (
     `user_id` int(10) unsigned not null,
     `title` varchar(255) not null,
     `body` text,
+    `created_datetime` datetime DEFAULT NULL,
+    `deleted_datetime` datetime DEFAULT NULL,
+    `deleted_user_id` datetime DEFAULT NULL,
+    `deleted_reason` datetime DEFAULT NULL,
     PRIMARY KEY (`short_story_id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
